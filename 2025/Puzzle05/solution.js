519948945bcd90eb7a66d6b52d9bdd005c273673
benjaminferreira <benjamin.j.ferreira@gmail.com>
2025-12-05 16:30:13 -0600

    Day 03 - Puzzle05: Highest joltage for battery banks

---
+++ 2025/Puzzle05/solution.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+joltageSum = 0;
+
+function ChooseBats(bank) {
+	bank = bank.trim();
+
+	// find first big numba
+	let bigDigit1 = 0;
+	let bigSpot1 = 0;
+	for (let i = 0; i < bank.length-1; i++) {
+		let curDigit = parseInt(bank[i]);
+		if (curDigit > bigDigit1) {
+			bigDigit1 = curDigit;
+			bigSpot = i;
+		}
+	}
+
+	// find second (start at first big number's spot + 1)
+	let bigDigit2 = 0;
+	for (let i = bigSpot1+1; i < bank.length; i++) {
+		let curDigit = parseInt(bank[i]);
+		if (curDigit > bigDigit2) {
+			bigDigit2 = curDigit;
+		}
+	}
+
+	joltageSum += parseInt("" + bigDigit1 + bigDigit2);
+}
+
+// Get file contents
+fs.readFile("input.txt", (err, data) => {
+	if (err) throw err;
+
+	// Split up data
+	const splitData = data.toString().split("\n");
+
+	splitData.forEach((bank) => ChooseBats(bank));
+	console.log("Sum of all joltages: " + joltageSum);
+});
